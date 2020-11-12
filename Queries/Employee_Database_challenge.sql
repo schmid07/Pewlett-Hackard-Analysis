0600d651252656150f2fa8c00643e9933fdb7d24
Bill Schmid <34810397+schmid07@users.noreply.github.com>
2020-11-12 10:41:02 -0600

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -5,12 +5,26 @@ SELECT
 	ti.title,
 	ti.from_date,
 	ti.to_date
-INTO retirement_titles
+--INTO retirement_titles
 FROM employees as e
 LEFT JOIN titles as ti
 ON e.emp_no = ti.emp_no
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY ti.emp_no;
+
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+--INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+SELECT COUNT(ut.emp_no), ut.title
+--INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY ut.count DESC;
 
 SELECT DISTINCT ON (e.emp_no) e.emp_no,
 	e.first_name,
